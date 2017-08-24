86e5245e85008da6de133eac60b8654117b049a0
Alvaro Santisteban <santisteban@karlmax-berlin.com>
2017-08-24 18:01:11 +0200

    feat: Display recipes on a list.

--- app/src/main/java/com/alvarosantisteban/bakingapp/RecipesActivity.java
+++ app/src/main/java/com/alvarosantisteban/bakingapp/RecipesActivity.java
@@ -3,10 +3,17 @@ package com.alvarosantisteban.bakingapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.alvarosantisteban.bakingapp.model.Recipe;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Arrays;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,19 +21,37 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
+/**
+ * Download the json with the recipes and displays them to the user.
+ */
 public class RecipesActivity extends AppCompatActivity {
 
     private static final String TAG = RecipesActivity.class.getSimpleName();
+
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
 
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        recyclerView = (RecyclerView) findViewById(R.id.recipes_list);
+
+        // TODO Set a GridLayout for tablet
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // TODO Ensure that the list position remains on orientation change
         downloadInitialJson();
     }
 
+    /**
+     * Downloads asynchronously the JSON with the list of recipes.
+     */
     private void downloadInitialJson() {
+        progressBar.setVisibility(View.VISIBLE);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(RecipesApi.RECIPES_BASE_URL)
                 .addConverterFactory(JacksonConverterFactory.create(new ObjectMapper()))
@@ -42,7 +67,10 @@ public class RecipesActivity extends AppCompatActivity {
                 if (response.body() != null) {
                     Recipe[] recipeList = response.body();
                     if (recipeList != null && recipeList.length > 0) {
-                        Log.d(TAG, "Recipes list downloaded: " + recipeList[0].getName());
+                        progressBar.setVisibility(View.GONE);
+
+                        RecipesAdapter adapter = new RecipesAdapter(Arrays.asList(recipeList), RecipesActivity.this);
+                        recyclerView.setAdapter(adapter);
                     }
                 }
             }
@@ -50,6 +78,8 @@ public class RecipesActivity extends AppCompatActivity {
             @Override
             public void onFailure(@NonNull Call<Recipe[]> call, @NonNull Throwable t) {
                 Log.e(TAG, "Error downloading or parsing recipes.", t);
+
+                Toast.makeText(RecipesActivity.this, R.string.error_download_json, Toast.LENGTH_SHORT).show();
             }
         });
     }
---
+++ app/src/main/java/com/alvarosantisteban/bakingapp/RecipesAdapter.java
@@ -0,0 +1,74 @@
+package com.alvarosantisteban.bakingapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alvarosantisteban.bakingapp.model.Recipe;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * The adapter for the list of recipes.
+ */
+class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipeViewHolder>{
+
+    private List<Recipe> recipes;
+    private Context context;
+
+    RecipesAdapter(List<Recipe> recipes, Context context) {
+        super();
+        this.recipes = recipes;
+        this.context = context;
+    }
+
+    @Override
+    public RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_item, parent, false);
+        return new RecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeViewHolder holder, int position) {
+        Recipe recipe = recipes.get(position);
+        holder.setName(recipe.getName());
+        holder.loadImage(recipe.getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes != null ? recipes.size() : 0;
+    }
+
+    class RecipeViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imageView;
+        private TextView name;
+
+        RecipeViewHolder(View itemView) {
+            super(itemView);
+
+            imageView = (ImageView) itemView.findViewById(R.id.recipe_image);
+            name = (TextView) itemView.findViewById(R.id.recipe_name);
+        }
+
+        void loadImage(String imageUrl) {
+            Glide
+                    .with(context)
+                    .load(imageUrl)
+                    .centerCrop()
+                    .placeholder(R.drawable.mortar_placeholder)
+                    .into(imageView);
+        }
+
+        void setName(String recipeName) {
+            name.setText(recipeName);
+        }
+    }
+}
